f61db9060009e514d72d70ac49d25f589f222fec
RaShubham <93131636+RaShubham@users.noreply.github.com>
2021-11-18 22:30:08 +0530

    Create server.js

---
+++ Assignment-1/server.js
@@ -0,0 +1,9 @@
+const http=require('http');
+const port=3000;
+
+http.createServer(function(req,resp){
+    resp.write("Welcome to node server demo");
+    resp.end();
+}).listen(port,function(){
+    console.log('Custom server is working at : http://localhost:${port}');
+})
